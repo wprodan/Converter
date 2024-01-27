f794f329dc23cf1c1b6a57ba7b0cbfab4b15e90c
wprodan <wprodan@ukr.net>
2024-01-27 09:20:51 +0300

    Add convert miles into kilometers

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,4 +1,18 @@
 package app;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double velInMiles;
+        double velInKm;
+        double result;
+        System.out.println("Vvedite chislo v milyah");
+        int a = scan.nextInt();
+        velInMiles = a;
+        velInKm = a*1.6;
+        System.out.println("Znachenie v kilometrah = " + velInKm);
+    }
+
 }
