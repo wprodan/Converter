809018faff8e803b1027072868ffe1d4b8b6bb8e
wprodan <wprodan@ukr.net>
2024-01-27 10:39:14 +0300

    Add choosing convert into kilometers or miles

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,5 +1,7 @@
 package app;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Scanner;
 
 public class Main {
@@ -8,11 +10,25 @@ public class Main {
         double velInMiles;
         double velInKm;
         double result;
-        System.out.println("Vvedite chislo v milyah");
-        int a = scan.nextInt();
-        velInMiles = a;
-        velInKm = a*1.6;
-        System.out.println("Znachenie v kilometrah = " + velInKm);
+        System.out.println("Vyberite perevod \n" +
+                "1. Miles into kilometers \n" +
+                "2. Kilometers into miles");
+        int vybor = scan.nextInt();
+        if (vybor == 1){
+            System.out.println("Vvedite chislo v milyah");
+            int a = scan.nextInt();
+            velInMiles = a;
+            velInKm = a*1.6;
+            System.out.println("Znachenie v kilometrah = " + velInKm);
+        } else if (vybor == 2) {
+            System.out.println("Vvedite chislo v kilometrah");
+            int a = scan.nextInt();
+            velInKm = a;
+            velInMiles = a/1.6;
+            System.out.println("Znachenie v milyah = " + velInMiles);
+        } else {
+            System.out.println("Nevernyy vybor");
+        }
     }
 
 }
